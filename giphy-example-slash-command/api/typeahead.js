c8b204414c5b87cc45526edd785a0aa6a7911202
Vishal Jain <vishaljain@college.harvard.edu>
2017-01-10 23:57:23 -0800

    take out code

--- giphy-example-slash-command/api/typeahead.js
+++ giphy-example-slash-command/api/typeahead.js
@@ -13,42 +13,6 @@ module.exports = function(req, res) {
     return;
   //}
 
-  /**request({
-    url: 'http://api.giphy.com/v1/gifs/search',
-    qs: {
-      q: term,
-      limit: 15,
-      api_key: key
-    },
-    gzip: true,
-    json: true,
-    timeout: 10 * 1000
-  }, function(err, response) {
-    if (err || response.statusCode !== 200 || !response.body || !response.body.data) {
-      res.status(500).send('Error');
-      return;
-    }
-
-    var results = _.chain(response.body.data)
-      .reject(function(image) {
-        return !image || !image.images || !image.images.fixed_height_small;
-      })
-      .map(function(image) {
-        return {
-          title: '<img style="height:75px" src="' + image.images.fixed_height_small.url + '">',
-          text: 'http://giphy.com/' + image.id
-        };
-      })
-      .value();
-
-    if (results.length === 0) {
-      res.json([{
-        title: '<i>(no results)</i>',
-        text: ''
-      }]);
-    } else {
-      res.json(results);
-    }
-  });**/
+  
 
 };
